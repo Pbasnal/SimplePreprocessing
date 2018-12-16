a2239c259c3bdd209d5db5e63af21278164a9acc
Pankaj Basnal <pbasnal@microsoft.com>
2018-12-16 11:37:16 +0530

    added sample_handling

--- SentimentFeatures.py
+++ SentimentFeatures.py
@@ -12,7 +12,7 @@ def create_lexicon(pos, neg):
         with open (fi, 'r') as f:
             contents = f.readlines()
             for l in contents[:hm_lines]:
-                all_words = word_tokenize(l)
+                all_words = word_tokenize(l.lower())
                 lexicon += list(all_words)
     
     lexicon = [lemmatizer.lemmatize(i) for i in lexicon]
@@ -24,3 +24,41 @@ def create_lexicon(pos, neg):
             l2.append(w)
 
     return l2
+
+def sample_handling(sample, lexicon, classification):
+    featureset = []
+
+    with open(sample, 'r') as f:
+        contents = f.readlines()
+        for l in contents[:hm_lines]:
+            current_words = word_tokenize(l.lower())
+            current_words = [lemmatizer.lemmatize(i) for i in current_words]
+            features = np.zeros(len(lexicon))
+            for word in current_words:
+                if word.lower() in lexicon:
+                    index_value = lexicon.index(word.lower())
+                    features[index_value] += 1
+            
+            features = list(features)
+            featureset.append([features, classification])
+    
+    return featureset
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
